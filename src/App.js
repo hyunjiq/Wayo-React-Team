aecf3672f1ed2f22e1de9ec0cd7cceb95ca64d30
tpgus2 <gksmfdnjs02@gmail.com>
2024-07-09 16:57:25 +0900

    data.json 업로드

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import data from './data/db.json';
+import Hd from "./component/Hd";
+
+//하나의 data 파일을 props로 전달하는 방식!!
+
+function Header() {
+    return (
+        <Hd/>
+    )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Hd from "./component/Hd";
 
+// font, scss
+import './fonts/font.css';
+import './scss/hd.scss';
 
+//부트스트랩, 스와이퍼 플러그인 연결해야함!
 
-
-
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Hd/>
+    <App/>
 );
 
