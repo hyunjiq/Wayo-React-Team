b52e1b70be4e57ccd7ec5377b0f6f3a8b628fdf4
hunjiq <edalnim12@gmail.com>
2024-07-17 11:53:48 +0900

    common.scss 각자 scss 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 // font, scss
-import './fonts/font.css';
-import './scss/hd.scss';
+import './fonts/fonts.css';
+import './scss/common.scss';
+import './scss/hj.scss';
+import './scss/je.scss';
+import './scss/sh.scss';
 
 //부트스트랩, 스와이퍼 플러그인 연결해야함!
 
@@ -12,6 +16,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
 
