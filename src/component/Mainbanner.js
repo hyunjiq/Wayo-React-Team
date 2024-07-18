5d4dd92c05e9c9f4677d30d326355c3f53a996ff
zueunjoeng <judms0314@gmail.com>
2024-07-18 17:34:46 +0900

    db수정 및 메인배너수정

--- src/component/Mainbanner.js
+++ src/component/Mainbanner.js
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide} from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
-
-import '../scss/je.scss';
-import mainbDB from '../data/db.json'
-import {Mainswiper, Mswiper, Ptext01,Ptext02,Ptext03,Ptext04 } from '../component/style'
+import 'swiper/css'; 
+import '../css/mainb.css';
+import mainbDB from '../data/mainbDB.json'
+import {Mainswiper, Mswiper, Ptext01,Ptext02,Ptext03,Ptext04 } from '../commonui/mainbui'
 
 const BannerSlider = () => {
     return (
@@ -14,87 +14,91 @@ const BannerSlider = () => {
             onSwiper={(swiper) => console.log(swiper)}
             modules={[Autoplay, Pagination]}
             autoplay={{
-                delay: 4000,
+                delay: 50000,
                 disableOnInteraction: false,
             }}
         >
-             {mainbDB["banner01"].map((v, i) => {
+            {mainbDB["banner01"].map((v, i) => {
+                const textArray = v.text.split('|');
+                const subtextArray = v.subtext.split('|');
+
                 return (
-                    <SwiperSlide>
-                     <Mainswiper key={i}>
-                        <Mswiper>
-                            <div className="mb-4">
-                                <Ptext02>{v.text[0]}</Ptext02>
-                                <Ptext02>{v.text[1]}</Ptext02>
-                            </div>
-                            <div className="mb-5">
-                                <Ptext03>{v.subtext[0]}</Ptext03>
-                                <Ptext03>{v.subtext[1]}</Ptext03>
-                            </div>
-                            <div><a href="javascript:void(0)" className="mainActing">유기동물 보호 활동</a></div>
-                         
-                        </Mswiper>   
-                        <div className="main_back"></div>
-                    </Mainswiper>
-               
-                </SwiperSlide>
-                );
-            })}
-            {mainbDB["banner02"].map((e, idx) => {
-                return (
-                    <SwiperSlide>
-                        <Mainswiper  key={idx}>
-                        <Mswiper>
-                            <div className="mb-4">
-                                <Ptext01>{e.text[0]}</Ptext01>
-                                <Ptext01>{e.text[1]}</Ptext01>
-                            </div>
-                            <div className="mb-5">
-                                <Ptext03>{e.subtext}</Ptext03>
-                            </div>
-                            <div><a href="javascript:void(0)" className="mainvideo">{e.btn}</a></div>
-                        </Mswiper>
-                        <div>
-                            <iframe
-                                key={idx}
-                                width="1242"
-                                height="700"
-                                className="mainbanner_Video"
-                                src="https://www.youtube.com/embed/RWRm1vHZN8A?si=KUt8OAz0PFLwfhsb"
-                                title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen
-                            ></iframe>
-                        </div>
+                    <SwiperSlide key={`banner01${i}`}>
+                        <Mainswiper>
+                            <Mswiper>
+                                <div className="mb-4">
+                                    <Ptext02>{textArray[0]}</Ptext02>
+                                    <Ptext02>{textArray[1]}</Ptext02>
+                                </div>
+                                <div className="mb-5">
+                                    <Ptext03>{subtextArray[0]}</Ptext03>
+                                    <Ptext03>{subtextArray[1]}</Ptext03>
+                                </div>
+                                <div><a href="javascript:void(0)" className="mainActing">{v.btn}</a></div>
+                            </Mswiper>
+                            <div className="main_back"></div>
                         </Mainswiper>
                     </SwiperSlide>
                 );
             })}
-             {mainbDB.banner03.map((b, index) => {
+            {mainbDB["banner02"].map((e, idx) => {
+                const textArray = e.text.split('|');
+
                 return (
-                    <SwiperSlide>
-                        <Mainswiper  key={index}>
-                        <Mswiper>
-                            <div className="mb-4">
-                                <img src="/img/img_je/main_collabo_txt.png" alt="콜라보로고" />
+                    <SwiperSlide key={`banner02${idx}`}>
+                        <Mainswiper>
+                            <Mswiper>
+                                <div className="mb-4">
+                                    <Ptext01>{textArray[0]}</Ptext01>
+                                    <Ptext01>{textArray[1]}</Ptext01>
+                                </div>
+                                <div className="mb-5">
+                                    <Ptext03>{e.subtext}</Ptext03>
+                                </div>
+                                <div><a href="javascript:void(0)" className="mainvideo">{e.btn}</a></div>
+                            </Mswiper>
+                            <div>
+                                <iframe
+                                    width="1242"
+                                    height="700"
+                                    className="mainbanner_Video"
+                                    src="https://www.youtube.com/embed/RWRm1vHZN8A?si=KUt8OAz0PFLwfhsb"
+                                    title="YouTube video player"
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin"
+                                    allowFullScreen
+                                ></iframe>
                             </div>
-                            <div className="mb-5">
-                                <Ptext01 className="mb-3">{b.text}</Ptext01>
-                                <Ptext04>{b.subtext[0]}</Ptext04>
-                                <Ptext04>{b.subtext[1]}</Ptext04>
-                                <Ptext04>{b.subtext[2]}</Ptext04>
+                        </Mainswiper>
+                    </SwiperSlide>
+                );
+            })}
+            {mainbDB.banner03.map((b, index) => {
+                const subtextArray = b.subtext.split('|');
+
+                return (
+                    <SwiperSlide key={`banner03${index}`}>
+                        <Mainswiper>
+                            <Mswiper>
+                                <div className="mb-4">
+                                    <img src="/img/main_collabo_txt.png" alt="콜라보로고" />
+                                </div>
+                                <div className="mb-5">
+                                    <Ptext01 className="mb-3">{b.text}</Ptext01>
+                                    <Ptext04>{subtextArray[0]}</Ptext04>
+                                    <Ptext04>{subtextArray[1]}</Ptext04>
+                                    <Ptext04>{subtextArray[2]}</Ptext04>
+                                </div>
+                            </Mswiper>
+                            <div>
+                                <img
+                                    src="/img/main_collabo.jpg"
+                                    alt="콜라보상품"
+                                    className="maincollabo"
+                                />
                             </div>
-                        </Mswiper>
-                        <div>
-                            <img
-                                src="/img/img_je/main_collabo.jpg"
-                                alt="콜라보상품"
-                                className="maincollabo"
-                            />
-                        </div>
-                    </Mainswiper>
+                        </Mainswiper>
                     </SwiperSlide>
                 );
             })}
