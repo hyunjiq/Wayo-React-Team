dd04f68db7740921e3d53ae3706bf6a8c27f026f
tpgus2 <gksmfdnjs02@gmail.com>
2024-08-06 15:41:34 +0900

    실시간 리뷰 반응형 및 지도 위치 수정 완료

--- src/component/LiveReveiw.js
+++ src/component/LiveReveiw.js
@@ -18,18 +18,44 @@ const Review = () => {
   }, []);
 
   useEffect(() => {
-    // Kakao Maps API 로드 후 실행
-    const loadMap = () => {
+    
+    const loadMap = () => { 
+      // Kakao Maps API 로드 후 실행
       const { kakao } = window;
-
+     
+      const centerLatLng = new kakao.maps.LatLng(37.52917322710707, 127.02080217910766); //종로구와 강남구 중심 임의로 지정
       const mapContainer = document.getElementById('map');
       const mapOption = {
-        center: new kakao.maps.LatLng(37.5665851, 126.9782038),
+        center: centerLatLng,
         level: 9
       };
 
       const map = new kakao.maps.Map(mapContainer, mapOption);
 
+      // 드래그 및 확대&스크롤 막기
+      map.setDraggable(false);
+      map.setZoomable(false);
+
+      const adjustMapCenter = () => {
+        const width = window.innerWidth;
+
+        // 너비에 따라 중심 좌표 조정
+        if (width > 700) {
+          const adjustedLng = 127.02580217910766; // 수동으로 설정한 중심 좌표
+          const adjustedCenter = new kakao.maps.LatLng(37.52917322710707, adjustedLng);
+          map.setCenter(adjustedCenter);
+        } else {
+          map.setCenter(centerLatLng);
+        }
+      };
+
+      // 초기 중심 조정
+      adjustMapCenter();
+
+      // 윈도우 리사이즈 이벤트에 대응하여 중심 조정
+      window.addEventListener('resize', adjustMapCenter);
+
+      
       const positions = [
         {
           title: '종로구',
@@ -37,9 +63,9 @@ const Review = () => {
           reviews: reviews.jongro
         },
         {
-          title: '동작구',
-          latlng: new kakao.maps.LatLng(37.512983, 126.939906),
-          reviews: reviews.dongjak
+          title: '강남구',
+          latlng: new kakao.maps.LatLng(37.487969454214145, 127.05658435821533),
+          reviews: reviews.gangnam
         }
       ];
 
@@ -60,6 +86,8 @@ const Review = () => {
       });
     };
 
+    
+
     if (window.kakao && window.kakao.maps) {
       loadMap();
     } else {
@@ -68,29 +96,32 @@ const Review = () => {
       script.onload = loadMap;
       document.head.appendChild(script);
     }
+
+    
   }, [reviews]);
 
 
   return (
-    <section id="live_reivew" className="overflow-hidden">
-      <h2 className="container">실시간 후기</h2>
-      <div className="d-flex justify-content-between align-items-center">
-        <div id="map" className="rv1 me-3"></div>
-        <div className="rv2">
-          <Link to="javascript:void(0)">
-            <p className='text-end'>지역별 더보기 +</p>
-          </Link>
-          <Swiper
+    <section id="live_reivew" className="container-lg">
+      <div className='d-flex justify-content-between align-items-center'>
+        <h2>실시간 후기</h2>  
+        <Link to="javascript:void(0)">
+          <p className='m-0 fw-semibold d-none d-md-block'>지역별 더보기 +</p>
+        </Link>
+      </div>
+    
+      <div className="d-lg-inline-flex d-lg-flex flex-md-column flex-lg-row img-fluid w-auto  align-items-center">
+        <div id="map" className="rv1 me-3 d-none d-md-block"></div>
+        <div className="rv2  d-none d-md-block">
+          <Swiper className="reviewswiper"
             loop={true}
             autoplay={{
               delay: 500,
               disableOnInteraction: false
             }}
             speed={15000}
-            slidesPerView={3}
-            spaceBetween={10}
+            slidesPerView={'auto'}
             allowTouchMove={false}
-            className="reviewswiper"
           >
             {currentReviews.slice(0, 6).map((review, index) => (
               <SwiperSlide key={`index${index}`} className="review">
@@ -109,10 +140,37 @@ const Review = () => {
               </SwiperSlide>
             ))}
           </Swiper>
+        </div> 
         </div>
-      </div>
+        
+          {
+            currentReviews.slice(0, 3).map((v,i)=>{
+              return(
+                <>
+                <div className='m_review d-md-none'>
+                  <div className="d-flex align-items-center">
+                    <img src={v.src} className="img" alt="프로필사진" />
+                    <div className="mx-4">
+                      <p className="nm">{v.nickname}</p>
+                      <img src="/img/img_sh/star.png" className="star" alt="별점" />
+                    </div>
+                  </div>
+                  <div className="my-3">
+                    <p>{v.region}</p>
+                    <p className="review_txt">{v.review}</p>
+                    <Link className='more_txt'>더보기</Link>
+                  </div>
+                </div> 
+                
+                </>
+              )
+              
+            })
+            
+          }
+          <Link to="javascript:void(0)" className="morebtn d-md-none">지역별 더보기 +</Link>
+       
     </section>
-
   );
 };
 
